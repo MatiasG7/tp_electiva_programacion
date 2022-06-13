93376844ae431c40f72afe8f9c7ed8b3aa582823
Sebastian Martinez <84688005+SebastianMartinezA@users.noreply.github.com>
2022-06-13 13:36:57 -0300

    Initial commit
    Implementacion basica, dos listboxes, actividades y comsiones

---
+++ WebApplication/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <asp:ListBox AutoPostBack="true"
+            ID="ListBoxActividades"
+            Rows="20"
+            Width="500px"
+            SelectionMode="Single"
+            runat="server" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged"></asp:ListBox>
+        <asp:ListBox AutoPostBack="true"
+            ID="ListBoxComisiones"
+            Rows="20"
+            Width="500px"
+            SelectionMode="Single"
+            runat="server" OnSelectedIndexChanged="ListBox2_SelectedIndexChanged"></asp:ListBox>
+    </div>
+
+</asp:Content>
---
+++ WebApplication/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CapaNegocio;
+
+
+namespace WebApplication
+{
+    public partial class _Default : Page
+    {
+        Club club = new Club();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ListBoxActividades.DataSource = club.Actividades;
+                ListBoxActividades.DataBind();
+
+            }
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Actividad act = club.Actividades[ListBoxActividades.SelectedIndex];
+            ListBoxComisiones.DataSource = act.Comisiones;
+            ListBoxComisiones.DataBind();
+        }
+
+        protected void ListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+}
